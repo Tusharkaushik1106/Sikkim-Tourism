7db9ed72a05239e887516833aba24aa942132493
Piyush-Fr <thakurpiyush3112005@gmail.com>
2025-09-12 16:06:57 +0530

    Second Commit

---
+++ App/lib/screens/main_shell.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:sikkim_app/screens/homescreen.dart';
+import 'package:sikkim_app/screens/maps.dart';
+import 'package:sikkim_app/screens/chatbot.dart';
+
+class MainShell extends StatefulWidget {
+  const MainShell({super.key});
+
+  @override
+  State<MainShell> createState() => _MainShellState();
+}
+
+class _MainShellState extends State<MainShell> {
+  int _index = 0;
+  final List<Widget> _screens = [
+    const Home(),
+    StreetViewExample(),
+    const Chatbot(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _index,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
+          BottomNavigationBarItem(icon: Icon(Icons.headset_mic), label: 'Guide'),
+        ],
+      ),
+    );
+  }
+}
+
+
+
---
+++ App/lib/services/support_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget{
+  static TextStyle headLineTextStyle(double size){
+    return TextStyle(
+      color: Colors.white,
+      fontSize: size,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Poppins',
+    );
+
+  }
+
+}
